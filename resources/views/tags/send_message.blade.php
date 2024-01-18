f4698a2bb89fc6cdccb6a46f2b2cf3438834f01a
germanmauro <mygsystems@gmail.com>
2024-01-17 22:16:00 -0300

    home page initial

--- resources/views/tags/send_message.blade.php
+++ resources/views/tags/send_message.blade.php
@@ -2,7 +2,8 @@
 <div>
       <p class="description-pink">!Encontraste un tag!</p>
       <p class="description-yellow">Enviale un mensaje al dueño para que pueda recuperarlo</p>
-      <a href="http://"></a><img class="h-auto max-w-40" src={{asset('images/whatsapp.png')}} /></a>
+      <a target="_blank" href="https://wa.me/{{$telephone}}?text=Encontré%20un%20objeto%20tuyo%20y%20quiero%20devolvertelo">
+        <img class="h-auto max-w-40" src={{asset('images/whatsapp.png')}} /></a>
       {{-- {{$telephone}} --}}
     </div>
 </x-guest-layout>
